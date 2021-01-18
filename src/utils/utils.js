7e0134bdf2b563896e8d8b5800f54026cb66415e
André Reis <andrereis992@gmail.com>
2021-01-18 00:33:40 +0000

    download person crew and cast

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,5 @@
 import { toast as Toast } from 'react-toastify';
+import pptxgen from "pptxgenjs";
 import Strings from './strings';
 
 const TOAST_CLOSETIME = 5000;
@@ -40,3 +41,38 @@ class Popup {
 }
 
 export const toast = new Popup();
+
+
+export const downloadPPT = (movies, title) => {
+	const pptx = new pptxgen();
+
+		for(const movie of movies) {
+			var slide = pptx.addSlide();
+			
+			slide.addText(
+				movie.Title || movie.title || movie.original_title || movie.name || movie.original_name,
+				{ x:0.25, y:0.25, w:'100%', h:1.5, fontSize:24, color:'0088CC' }
+			);
+
+			{movie.Director && slide.addText(
+			   movie.Director,
+			   { x:0.25, y:1.5, w:'100%', h:0.5, fontSize:14, color:'878787' }
+			)}
+
+			slide.addText(
+			   movie.release_date || movie.first_air_date,
+			   { x:1.25, y:1.5, w:'100%', h:0.5, fontSize:14, color:'878787' }
+			)
+
+			slide.addText(
+				movie.overview,
+				{ x:0.0, y:2.45, w:'100%', h:3.1, align:'center', justifyContent:'start' , fontSize:16, color:'878787' }
+			)
+
+			const poster = movie.poster_path || movie.backdrop_path
+
+			{poster && slide.addImage({ path:`https://image.tmdb.org/t/p/original/${poster}`, x:7.5, y:0.25, w:1.6, h:2.2 })}
+		}
+
+		pptx.writeFile(title);
+}
